a77330f915cd0f3a50e58ae3305645e5845a7134
Sanzhar <hurripan@gmail.com>
2025-01-17 15:39:10 +0600

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// filepath: /c:/Users/BlackHole/Documents/test_task_green_api/public/config.js
+const CONFIG = {
+    API_URL: 'https://7105.api.greenapi.com',
+    MEDIA_URL: 'https://7105.media.greenapi.com',
+    ID_INSTANCE: '7105175089',
+    API_TOKEN_INSTANCE: '0cd19ecad3cf4971bdb1533eaf5853aa912782109b9d45a5ab'
+};
